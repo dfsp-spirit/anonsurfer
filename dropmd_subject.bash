e1c117c6ebf9978e6727e20525f89314df9fc35c
Tim Schaefer <ts+code@rcmd.org>
2020-02-07 12:41:54 +0100

    convert MGH back from NIFTI, handle tags in surface files by GIFTI conversion

--- dropmd_subject.bash
+++ dropmd_subject.bash
@@ -50,46 +50,84 @@ echo "$APPTAG INFO: --- Dropping metadata for subject '${SUBJECT_ID}' in directo
 
 
 ## --------------------------------- Handle metadata in files in mri/ dir ---------------------------------------
+
+## ------------- Handle MGZ volume files  -----------
+
 echo "$APPTAG INFO: Handling data in sub directory 'mri' for subject '${SUBJECT_ID}'." >> "${LOGFILE}"
 VOLUME_FILES=$(find "$SD/mri/" -name '*.mgz');
 for VOL_FILE in $VOLUME_FILES; do
   if [ ! -f "${VOL_FILE}" ]; then
-      echo "$APPTAG NOTICE: Subject '${SUBJECT_ID} has no file '${VOL_FILE}'. Continuing." >> "${LOGFILE}"
+      echo "$APPTAG NOTICE: Subject '${SUBJECT_ID} has no volume file '${VOL_FILE}'. Continuing." >> "${LOGFILE}"
       continue
   fi
   NOMD_FILE="${VOL_FILE}.nii"
   echo "$APPTAG INFO: * Handling subject '${SUBJECT_ID}' metadata in volume file '$VOL_FILE'." >> "${LOGFILE}"
   mri_convert "${VOL_FILE}" "${NOMD_FILE}"
   if [ $? -ne 0 ]; then
-      echo "$APPTAG ERROR: mri_convert command failed for subject '${SUBJECT_ID}' file '${VOL_FILE}'. Exiting." >> "${LOGFILE}"
-      exit 1
+      echo "$APPTAG ERROR: mri_convert command failed for subject '${SUBJECT_ID}' volume file '${VOL_FILE}' (MGZ to NIFTI)." >> "${LOGFILE}"
   else
       if [ -f "${NOMD_FILE}" ]; then
-          mv "${NOMD_FILE}" "${VOL_FILE}"
+          mri_convert "${NOMD_FILE}" "${VOL_FILE}"
           if [ $? -ne 0 ]; then
-              echo "$APPTAG ERROR: Could not rename subject '${SUBJECT_ID}' volume file '${NOMD_FILE}' to '${VOL_FILE}'. Exiting." >> "${LOGFILE}"
-              exit 1
+              echo "$APPTAG ERROR: Could not convert subject '${SUBJECT_ID}' volume file '${NOMD_FILE}' back to '${VOL_FILE}' (NIFTI to MGZ)." >> "${LOGFILE}"
           else
-              echo "$APPTAG INFO: Successfully defaced subject '${SUBJECT_ID}' brain volume '${VOL_FILE}'." >> "${LOGFILE}"
+              rm "${NOMD_FILE}"   # delete temporary NIFTI file
+              echo "$APPTAG INFO: Successfully dropped metadata in subject '${SUBJECT_ID}' brain volume '${VOL_FILE}'." >> "${LOGFILE}"
           fi
       else
-          echo "$APPTAG ERROR: Cannot read subject '${SUBJECT_ID}' defaced filed '${NOMD_FILE}' after mri_convert command (even though it returned no error). Exiting." >> "${LOGFILE}"
-          exit 1
+          echo "$APPTAG ERROR: Cannot read subject '${SUBJECT_ID}' no metadata NIFTI volume file '${NOMD_FILE}' after mri_convert command (even though it returned no error). MD not dropped." >> "${LOGFILE}"
       fi
   fi
 done
 
 
 ## --------------------------------- Handle metadata in files in label/ dir ---------------------------------------
+
+## ------------- Handle ASCII label files  -----------
 echo "$APPTAG INFO: Handling data in sub directory 'label' for subject '${SUBJECT_ID}'." >> "${LOGFILE}"
 LABEL_FILES=$(find "$SD/label/" -name '*.label');
 for LABEL_FILE in $LABEL_FILES; do
     echo "$APPTAG INFO: Handling subject '${SUBJECT_ID}' ASCII label file '$LABEL_FILE'." >> "${LOGFILE}"
     sed --in-place "1s/.*/#! ascii label for anon subject/" "${LABEL_FILE}" >> "${LOGFILE}"
     if [ $? -ne 0 ]; then
-        echo "$APPTAG ERROR: sed command failed for subject '${SUBJECT_ID}' label file '${LABEL_FILE}'. Exiting." >> "${LOGFILE}"
-        exit 1
+        echo "$APPTAG ERROR: sed command failed for subject '${SUBJECT_ID}' label file '${LABEL_FILE}'." >> "${LOGFILE}"
     fi
+done
+
+## --------------------------------- Handle metadata in files in surf/ dir ---------------------------------------
+
+## ------------- Handle surface files  -----------
+echo "$APPTAG INFO: Handling data in sub directory 'surf' for subject '${SUBJECT_ID}'." >> "${LOGFILE}"
+# This is ugly: the surface files cannot be identified by their file extension (they have none), so we need to know them. If
+# you manually created additional surfaces, you will have to add them here.
+SURFACES="white pial inflated orig smoothwm orig.nofix inflated.nofix pial-outer-smoothed qsphere.nofix sphere"
+HEMIS="lh rh"
+for SURFACE in $SURFACES; do
+    for HEMI in $HEMIS; do
+        SURFACE_FILE="$SD/surf/${HEMI}.${SURFACE}"
+        if [ -f "${SURFACE_FILE}" ]; then
+            echo "$APPTAG INFO: * Handling subject '${SUBJECT_ID}' metadata in surface file '$SURFACE_FILE'." >> "${LOGFILE}"
+            NOMD_FILE="${SURFACE_FILE}.gii"
+            mris_convert "${SURFACE_FILE}" "${NOMD_FILE}"
+            if [ $? -ne 0 ]; then
+                echo "$APPTAG ERROR: mris_convert command failed for subject '${SUBJECT_ID}' surface file '${SURFACE_FILE}' (fssurf to GIFTI)." >> "${LOGFILE}"
+            else
+                if [ -f "${NOMD_FILE}" ]; then
+                    mris_convert "${NOMD_FILE}" "${SURFACE_FILE}"
+                    if [ $? -ne 0 ]; then
+                        echo "$APPTAG ERROR: Could not convert subject '${SUBJECT_ID}' GIFTI surface file '${NOMD_FILE}' back to '${SURFACE_FILE}' (GIFTI to fssurf)." >> "${LOGFILE}"
+                    else
+                        rm "${NOMD_FILE}" # delete temporary GIFTI file
+                        echo "$APPTAG INFO: Successfully dropped metadata in subject '${SUBJECT_ID}' brain surface '${SURFACE_FILE}'." >> "${LOGFILE}"
+                    fi
+                else
+                    echo "$APPTAG ERROR: Cannot read subject '${SUBJECT_ID}' no metadata GIFTI surface file '${NOMD_FILE}' after mris_convert command (even though it returned no error). MD not dropped." >> "${LOGFILE}"
+                fi
+            fi
+        else
+            echo "$APPTAG NOTICE: subject '${SUBJECT_ID}' has no surface file for surface '$SURFACE' hemi '$HEMI' at '${SURFACE_FILE}'." >> "${LOGFILE}"
+        fi
+    done
 done
 
 echo "$APPTAG INFO: Finished metadata dropping for subject '${SUBJECT_ID}'." >> "${LOGFILE}"
