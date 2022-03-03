912f8698bb76187fd7ccce7a644b31a9b7bb5d6e
Tim Schaefer <ts+code@rcmd.org>
2022-03-03 09:51:44 +0100

    more details in subject log files

--- deface_subject.bash
+++ deface_subject.bash
@@ -10,7 +10,7 @@ APPTAG="[DEFACE_SUBJECT]"
 
 # Parse command line arguments
 if [ -z "$2" ]; then
-  echo "$APPTAG ERROR: Arguments missing. Exiting."
+  echo "$APPTAG DEF_ERROR: Arguments missing. Exiting."
   echo "$APPTAG INFO: Usage: $0 <subject_id> <subjects_dir> [<log_tag>]"
   echo "$APPTAG INFO: This script requires that the FREESURFER_HOME environment variable is configured."
   echo "$APPTAG WARNING: +++++ Running this script will alter parts of your imaging data! +++++ "
@@ -34,39 +34,39 @@ LOGFILE="anonsurfer_subject_deface_${SUBJECT_ID}_${DATE_TAG}.log"
 #### check some basic stuff first
 
 if [ ! -d "${SUBJECTS_DIR}" ]; then
-  echo "$APPTAG ERROR: Parameter 'subjects_dir' points to '${SUBJECTS_DIR}' but that directory does NOT exist. Exiting." >> "${LOGFILE}"
+  echo "$APPTAG DEF_ERROR: Parameter 'subjects_dir' points to '${SUBJECTS_DIR}' but that directory does NOT exist. Exiting." >> "${LOGFILE}"
   exit 1
 fi
 
 
 if [ ! -d "${SUBJECTS_DIR}/${SUBJECT_ID}" ]; then
-  echo "$APPTAG ERROR: Directory for subject '${SUBJECT_ID}' not found in SUBJECTS_DIR '${SUBJECTS_DIR}'. Exiting." >> "${LOGFILE}"
+  echo "$APPTAG DEF_ERROR: Directory for subject '${SUBJECT_ID}' not found in SUBJECTS_DIR '${SUBJECTS_DIR}'. Exiting." >> "${LOGFILE}"
   exit 1
 fi
 
 
 if [ -z "${FREESURFER_HOME}" ]; then
-    echo "$APPTAG ERROR: Environment variable FREESURFER_HOME not set, cannot find deface template volumes. Exiting." >> "${LOGFILE}"
+    echo "$APPTAG DEF_ERROR: Environment variable FREESURFER_HOME not set, cannot find deface template volumes. Exiting." >> "${LOGFILE}"
     exit 1
 fi
 
 #### Check deface tools
 SKULL_TEMPLATE="${FREESURFER_HOME}/average/talairach_mixed_with_skull.gca"
 if [ ! -f "${SKULL_TEMPLATE}" ]; then
-    echo "$APPTAG ERROR: skull template for defacing not found at '${SKULL_TEMPLATE}'. Exiting." >> "${LOGFILE}"
+    echo "$APPTAG DEF_ERROR: skull template for defacing not found at '${SKULL_TEMPLATE}'. Exiting." >> "${LOGFILE}"
     exit 1
 fi
 
 FACE_TEMPLATE="${FREESURFER_HOME}/average/face.gca"
 if [ ! -f "${FACE_TEMPLATE}" ]; then
-    echo "$APPTAG ERROR: face template for defacing not found at '${FACE_TEMPLATE}'. Exiting." >> "${LOGFILE}"
+    echo "$APPTAG DEF_ERROR: face template for defacing not found at '${FACE_TEMPLATE}'. Exiting." >> "${LOGFILE}"
     exit 1
 fi
 
 #### ok, lets go
 
 SD="${SUBJECTS_DIR}/${SUBJECT_ID}";
-echo "$APPTAG INFO: --- Defacing subject '${SUBJECT_ID}' in directory '${SD}'. ---" >> "${LOGFILE}"
+echo "$APPTAG DEF_INFO: --- Defacing subject '${SUBJECT_ID}' in directory '${SD}'. ---" >> "${LOGFILE}"
 
 
 # We cannot simply search all volume files, as mri_deface will fail for volumes which do not resemble a full head.
@@ -78,33 +78,41 @@ NUM_EXISTING=0
 NUM_OK=0
 NUM_MISSING=0
 NUM_FAILED=0
+WHICH_MISSING=""
+WHICH_FAILED=""
+WHICH_OK=""
 for REL_VOL_FILE in $VOLUME_FILES_RELATIVE_TO_MRI_DIR; do
     VOL_FILE="${SD}/${REL_VOL_FILE}"
     if [ ! -f "${VOL_FILE}" ]; then
-        echo "$APPTAG NOTICE: Subject '${SUBJECT_ID} has no file '${VOL_FILE}'. Continuing." >> "${LOGFILE}"
+        echo "$APPTAG DEF_NOTICE: Subject '${SUBJECT_ID} has no file '${VOL_FILE}'. Continuing." >> "${LOGFILE}"
         NUM_MISSING=$((NUM_MISSING+1))
+        WHICH_MISSING="${WHICH_MISSING} ${REL_VOL_FILE}"
         continue
     fi
     NUM_EXISTING=$((NUM_EXISTING+1))
     DEFACED_FILE="${VOL_FILE}.defaced.mgz"   # temp name for defaced file, will be renamed to original file name later.
-    echo "$APPTAG INFO: * Handling subject '${SUBJECT_ID}' volume file '$VOL_FILE'."
+    echo "$APPTAG DEF_INFO: * Handling subject '${SUBJECT_ID}' volume file '$VOL_FILE'."
     mri_deface "${VOL_FILE}" "${SKULL_TEMPLATE}" "${FACE_TEMPLATE}" "${DEFACED_FILE}" >> "${LOGFILE}" 2>&1
     if [ $? -ne 0 ]; then
-        echo "$APPTAG ERROR: mri_deface command failed for subject '${SUBJECT_ID}' file '${VOL_FILE}'. Subject not defaced." >> "${LOGFILE}"
+        echo "$APPTAG DEF_ERROR: mri_deface command failed for subject '${SUBJECT_ID}' file '${VOL_FILE}'. Subject not defaced." >> "${LOGFILE}"
         NUM_FAILED=$((NUM_FAILED+1))
+        WHICH_FAILED="${WHICH_FAILED} ${REL_VOL_FILE}"
     else
         if [ -f "${DEFACED_FILE}" ]; then
             mv "${DEFACED_FILE}" "${VOL_FILE}"
             if [ $? -ne 0 ]; then
-                echo "$APPTAG ERROR: Could not rename subject '${SUBJECT_ID}' defaced file '${DEFACED_FILE}' to '${VOL_FILE}'. Subject not defaced." >> "${LOGFILE}"
+                echo "$APPTAG DEF_ERROR: Could not rename subject '${SUBJECT_ID}' defaced file '${DEFACED_FILE}' to '${VOL_FILE}'. Subject not defaced." >> "${LOGFILE}"
                 NUM_FAILED=$((NUM_FAILED+1))
+                WHICH_FAILED="${WHICH_FAILED} ${REL_VOL_FILE}"
             else
-                echo "$APPTAG INFO:  Successfully defaced subject '${SUBJECT_ID}' brain volume '${VOL_FILE}'." >> "${LOGFILE}"
+                echo "$APPTAG DEF_INFO:  Successfully defaced subject '${SUBJECT_ID}' brain volume '${VOL_FILE}'." >> "${LOGFILE}"
                 NUM_OK=$((NUM_OK+1))
+                WHICH_OK="${WHICH_OK} ${REL_VOL_FILE}"
             fi
         else
-            echo "$APPTAG ERROR: Cannot read subject '${SUBJECT_ID}' defaced file '${DEFACED_FILE}' after mri_deface command (even though it returned no error). Subject not defaced." >> "${LOGFILE}"
+            echo "$APPTAG DEF_ERROR: Cannot read subject '${SUBJECT_ID}' defaced file '${DEFACED_FILE}' after mri_deface command (even though it returned no error). Subject not defaced." >> "${LOGFILE}"
             NUM_FAILED=$((NUM_FAILED+1))
+            WHICH_FAILED="${WHICH_FAILED} ${REL_VOL_FILE}"
         fi
     fi
 done
@@ -115,8 +123,10 @@ else
     STATUS="STATUS_CHECK_ISSUES"
 fi
 
-echo "$APPTAG INFO: Subject '${SUBJECT_ID}' details: ${NUM_TRIED} volume files checked, ${NUM_EXISTING} found (${NUM_MISSING} missing), ${NUM_OK} successfully defaced, ${NUM_FAILED} failed." >> "${LOGFILE}"
-
+echo "$APPTAG INFO: Subject '${SUBJECT_ID}' final details: ${NUM_TRIED} volume files checked, ${NUM_EXISTING} found (${NUM_MISSING} missing), ${NUM_OK} successfully defaced, ${NUM_FAILED} failed." >> "${LOGFILE}"
+echo "$APPTAG INFO: Subject '${SUBJECT_ID}' final file status: FILES_OK=$WHICH_OK"
+echo "$APPTAG INFO: Subject '${SUBJECT_ID}' final file status: FILES_MISSING=$WHICH_MISSING"
+echo "$APPTAG INFO: Subject '${SUBJECT_ID}' final file status: FILES_FAILED=$WHICH_FAILED"
 # The following report lines are in a stable format that is designed to be be easily parsable, e.g., using 'grep'.
 echo "$APPTAG INFO: [REPORT] Subject '${SUBJECT_ID}' DEFACE_FAIL_COUNT=${NUM_FAILED}" >> "${LOGFILE}"
 echo "$APPTAG INFO: [REPORT] Subject '${SUBJECT_ID}' DEFACE_FINAL_STATUS=${STATUS}" | tee -a "${LOGFILE}"
