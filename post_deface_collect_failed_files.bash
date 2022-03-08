e2cb7317b62ec9de67099a78d674734b12dccd73
Tim Schaefer <ts+code@rcmd.org>
2022-03-08 10:18:40 +0100

    add util script to collect all failed files

---
+++ post_deface_collect_failed_files.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# When defacing subjects in parallel with the run_deface.bash pipeline, failed files get logged
+# to one log file per subject. This script collects all failed files, by simply concatenating the
+# subject log files.
+#
+# Written by TS, 2022-02-08.
+
+subjects_file="$1"
+subjects_dir="$2"
+all_log_file="deface_all_failed_files.log"
+
+APPTAG="[COLLECT_DEF_FAILED]"
+
+if [ -z "$subjects_dir" ]; then
+    echo "$APPTAG Collect all files that failed defacing in file '${all_log_file}'."
+    echo "$APPTAG Usage: $0 <subjects_file> <subjects_dir>"
+    echo "$APPTAG    <subjects_file> : path to a textfile containing one subject per line"
+    echo "$APPTAG    <subjects_dir>  : path to the FreeSurfer recon-all output directory (known as FreeSurfer SUBJECTS_DIR)."
+fi
+
+subjects_list=$(cat "${subjects_file}" | tr -d '\r' | tr '\n' ' ')    # fix potential windows line endings (delete '\r') and replace newlines by spaces as we want a list
+
+num_subjects_okay=0
+num_subjects_failed=0
+for subject in $subjects_list; do
+    subject_failed_files_log="failed_files_anonsurfer_subject_deface_${subject}.log"
+    if [ -f "${subject_failed_files_log}" ]; then
+        num_subjects_failed=$((num_subjects_failed+1))
+        cat "${subject_failed_files_log}" >> ${all_log_file}
+    else
+        num_subjects_okay=$((num_subjects_okay+1))
+    fi
+done
+
+echo "$APPTAG Collected failed files for $num_subjects_failed subjects in '${all_log_file}'. $num_subjects_okay subjects did not have any failed files."
